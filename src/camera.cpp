d2bdd2eb602be6304ddd27cfcb94d4a8dc8b71fd
Daniel Victoriano <djose1164@gmail.com>
2022-06-27 17:23:29 +0000

    Fix typo.

--- src/camera.cpp
+++ src/camera.cpp
@@ -1,12 +1,12 @@
 #include "camera.h"
 
-XM_Camera::XM_Camera() : pos{0, 0, 0}, target{0, 1.f, 0}, up{0, 1.f, 0}, fovy{45.f}
+XM_Camera::XM_Camera() : pos{0, 1, 0}, target{0, 1.f, 0}, up{0, 1.f, 0}, fovy{45.f}
 {
 	camera_.position = pos;
 	camera_.target = target;
 	camera_.up = up;
-	cmaera_.fovy = fovy;
-	cmaera_.projection = CAMERA_PERSPECTIVE;
+	camera_.fovy = fovy;
+	camera_.projection = CAMERA_PERSPECTIVE;
 }
 
 XM_Camera::XM_Camera(const Vector3 &pos) : pos{pos}, target{0, 1.f, 0}, up{0, 1.f, 0}, 
@@ -25,13 +25,15 @@ XM_Camera::XM_Camera(const Vector3 &pos, const Vector3 &target, const Vector3 &u
 		   fovy{fovy}
 {}
 
-inline void draw(const void *todraw)
+void XM_Camera::draw(const void *todraw)
 {
+	Vector3 cubePosition = {0.f, 0.f, 0.f};
+	TraceLog(LOG_INFO, "Camera position {%.3f:%.3f:%.3f}", camera_.position.x, camera_.position.y, camera_.position.z);
 	BeginMode3D(camera_);
 		DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
 		DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
 		DrawGrid(10, 1.0f);
-	EndMode3D;
+	EndMode3D();
 }
 	
 XM_Camera::~XM_Camera()
